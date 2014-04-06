91e71e53b423564125db464f8b611ed250731bbc
Smitha Milli <smitha.milli@gmail.com>
2014-04-06 00:48:37 -0700

    save horizontal / vertical lines

--- test.py
+++ test.py
@@ -1,22 +1,23 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('img/phone.jpg')
+img = cv2.imread('img/sobelcropped.png')
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 edges = cv2.Canny(gray,100,100,apertureSize = 3)
 
 lines = cv2.HoughLines(edges,1,np.pi/180,180)
-#lines = np.array(filter(lambda x: np.floor(x[1] / (np.pi/4)) < DIFFERENCE))
 
 # only use horizontal/vertical lines
-def valid_angle(theta):
+def valid_line(theta):
     DIFFERENCE = np.pi/70
     ninety = np.pi/2;
     num = theta / ninety - np.floor(theta / ninety)
     return num * (np.pi/2) < DIFFERENCE
 
+horizontal_lines = []
+vertical_lines = []
 for rho,theta in lines[0]:
-    if (valid_angle(theta)):
+    if (valid_line(theta)):
         a = np.cos(theta)
         b = np.sin(theta)
         x0 = a*rho
@@ -26,7 +27,12 @@ for rho,theta in lines[0]:
         x2 = int(x0 - 1000*(-b))
         y2 = int(y0 - 1000*(a))
 
+        if x1 < -900 and x2 > 900:
+          horizontal_lines.append([(x1, y1), (x2, y2)])
+        if y1 > 900 and y2 < -900:
+          vertical_lines.append([(x1, y1), (x2, y2)])
         cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
+
 
 
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)

9bd5d1322b9c3d41a0db87a42f13228a076c5af3
Davis Foote <djfoote@berkeley.edu>
2014-04-06 04:46:17 -0700

    Finished Square class

--- Square.py
+++ Square.py
@@ -3,30 +3,39 @@ import cv2;
 class Square():
 	SOBEL_MIN = 10;
 
-	def __init__(self, sobel_img, color_img, x, y):
+	def __init__(self, sobel_img, color_img, x, y, boardinfo):
+		self.boardinfo = boardinfo
 		self.sobel_img = sobel_img
 		self.color_img = color_img
 		self.x = x
 		self.y = y
+		self.square_color = self.boardinfo['color'][(self.x + self.y) % 2];
 
-		self.color = None;
-		self.exists = self.sobel_detect()
-		if self.exists:
-		  	self.color = color_detect()
-
-	def sobel_detect(self):
+	@property
+	def has_piece(self):
 		avg = self.sobel_img[10:-10, 10:-10].mean()
-		self.exists = avg > Square.SOBEL_MIN
-		# self.color_avg
+		self.has_piece = avg > Square.SOBEL_MIN
+		return self.has_piece
 
-	def color_detect(self, infopacket):
-		color_average()
-		self.infopacket = infopacket
-		color = infopacket['color'][(self.x + self.y) % 2]
-		if color == "W":
-			diffW = infopacket['']
-			diffB = infopacket['']
-			#YO, FIX THIS
+	@property
+	def piece_color(self):
+		if not self.has_piece:
+			return False
+		if self.square_color == 'W':
+			if self.w_w_diff < self.b_w_diff:
+				# White piece on a white square
+				self.piece_color = 'W'
+			else:
+				# Black piece on a white square
+				self.piece_color = 'B'
+		else:
+			if self.w_b_diff < self.b_b_diff:
+				# White_piece on a black square
+				self.piece_color = 'W'
+			else:
+				# Black piece on a black square
+				self.piece_color = 'B'
+		return self.piece_color
 
 	@property
 	def color_average(self):
@@ -39,12 +48,42 @@ class Square():
 	@property
 	def sobel_average(self):
 		self.sobel_average = \
-				(self.sobel_img[:,:,0].mean(), 
-			 	 self.sobel_img[:,:,1].mean(), 
-				 self.sobel_img[:,:,2].mean())
+				(self.sobel_img[:, :, 0].mean(), 
+			 	 self.sobel_img[:, :, 1].mean(), 
+				 self.sobel_img[:, :, 2].mean())
 		return self.sobel_average
 
+	@property
+	def w_w_diff(self):
+		self.w_w_diff = color_diff(self.color_average, self.boardinfo['w_w'])
+		return self.w_w_diff
+
+	@property
+	def b_w_diff(self):
+		self.w_w_diff = color_diff(self.color_average, self.boardinfo['b_w'])
+		return self.b_w_diff
+
+	@property
+	def w_b_diff(self):
+		self.w_b_diff = color_diff(self.color_average, self.boardinfo['w_b'])
+		return self.w_b_diff
+
+	@property
+	def b_b_diff(self):
+		self.b_b_diff = color_diff(self.color_average, self.boardinfo['b_b'])
+		return self.b_b_diff
+	
+	def __repr__(self):
+		string = str((self.x, self.y)) + ": " + str(self.has_piece)
+		if self.has_piece:
+			string += ", " + str(self.piece_color)
+		return string
+
+def color_diff(tup1, tup2):
+	total = 0
+	for i in range(min(len(tup1), len(tup2))):
+		total += abs(tup1[i] - tup2[i])
 
 if __name__ == '__main__':
 	s = Square( cv2.imread('img/chessboard.jpg'), 
-				cv2.imread('img/chessboard.jpg'), 1, 2)
+				cv2.imread('img/chessboard.jpg'), 1, 2, {'color':['W', 'B']})

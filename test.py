8ca7e3f2810acfdc24118567b6ba396d59a7ae6c
Smitha Milli <smitha.milli@gmail.com>
2014-04-06 02:03:46 -0700

    clean up code

--- test.py
+++ test.py
@@ -2,10 +2,12 @@ import cv2
 import numpy as np
 
 img = cv2.imread('img/sobelcropped.png')
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-edges = cv2.Canny(gray,50,175,apertureSize = 3)
+horizontal_lines, vertical_lines = hough_lines(img)
 
-lines = cv2.HoughLines(edges,1,np.pi/180,180)
+# more lines than necessary, so merge
+if (len(vertical_lines) * len(horizontal_lines) > 49):
+    merge_lines(vertical_lines)
+    merge_lines(horizontal_lines)
 
 # only use horizontal/vertical lines
 def valid_line(theta):
@@ -14,24 +16,29 @@ def valid_line(theta):
     num = theta / ninety - np.floor(theta / ninety)
     return num * (np.pi/2) < DIFFERENCE
 
-horizontal_lines = []
-vertical_lines = []
-for rho,theta in lines[0]:
-    if (valid_line(theta)):
-        a = np.cos(theta)
-        b = np.sin(theta)
-        x0 = a*rho
-        y0 = b*rho
-        x1 = int(x0 + 1000*(-b))
-        y1 = int(y0 + 1000*(a))
-        x2 = int(x0 - 1000*(-b))
-        y2 = int(y0 - 1000*(a))
+def hough_lines(img):
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(gray,50,175,apertureSize = 3)
+    lines = cv2.HoughLines(edges,1,np.pi/180,180)
+    horizontal_lines = []
+    vertical_lines = []
+    for rho,theta in lines[0]:
+        if (valid_line(theta)):
+            a = np.cos(theta)
+            b = np.sin(theta)
+            x0 = a*rho
+            y0 = b*rho
+            x1 = int(x0 + 1000*(-b))
+            y1 = int(y0 + 1000*(a))
+            x2 = int(x0 - 1000*(-b))
+            y2 = int(y0 - 1000*(a))
 
-        if x1 < -900 and x2 > 900:
-          horizontal_lines.append({'rho': rho, 'theta': theta, 'p1': (x1, y1), 'p2': (x2, y2)})
-        if y1 > 900 and y2 < -900:
-          vertical_lines.append({'rho': rho, 'theta': theta, 'p1': (x1, y1), 'p2': (x2, y2)})
-        #cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
+            if x1 < -900 and x2 > 900:
+              horizontal_lines.append({'rho': rho, 'theta': theta, 'p1': (x1, y1), 'p2': (x2, y2)})
+            if y1 > 900 and y2 < -900:
+              vertical_lines.append({'rho': rho, 'theta': theta, 'p1': (x1, y1), 'p2': (x2, y2)})
+            #cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
+    return (horizontal_lines, vertical_lines)
 
 THETA_DIFF = np.pi/1
 MAGNITUDE_DIFF = 25
@@ -53,30 +60,13 @@ def merge_lines(lines):
                 print('similar')
             j+=1
         i+=1
-    print(len(lines))
 
-# more lines than necessary, so merge
-if (len(vertical_lines) * len(horizontal_lines) > 49):
-    merge_lines(vertical_lines)
-    merge_lines(horizontal_lines)
-
+# display lines
 for vert_line in vertical_lines:
     cv2.line(img,vert_line['p1'],vert_line['p2'],(0,0,255),2)
 
 for hori_line in horizontal_lines:
     cv2.line(img,hori_line['p1'],hori_line['p2'],(0,0,255),2)
-        
-
 
 def findIntersection(line1, line2):
     pass
-
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-dst = cv2.cornerHarris(gray,3,3,0.04)
-#result is dilated for marking the corners, not important
-dst = cv2.dilate(dst,None)
-
-# Threshold for an optimal value, it may vary depending on the image.
-img[dst>0.01*dst.max()]=[255,0,0]
-
-cv2.imwrite('houghlines3.jpg',img)
